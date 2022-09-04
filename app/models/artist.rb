48cb077e597ad74017f6a8b3dd139177e57f3f72
Allan <kipropsallan@gmail.com>
2022-09-04 13:17:56 +0300

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -13,4 +13,17 @@ class Artist < ActiveRecord::Base
   def genre_count
     #return the number of genres associated with the artist
   end
+
+  def get_genre_of_first_song
+    self.songs.first.genre
+  end
+  
+  def song_count
+    self.songs.count
+  end
+
+  def genre_count
+    self.genres.count
+  end
+  
 end
